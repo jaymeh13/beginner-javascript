49f2028084e3c1f140ac55cf9b0e19d503395c40
Jayme Hoffman <jaymeh13@gmail.com>
2024-07-22 15:24:23 -0500

    Completed types

--- playground/types.js
+++ playground/types.js
@@ -1,20 +1,52 @@
-debugger;
-const name = 'wes';
-const middle = 'topher';
-const last = 'bos';
+// debugger;
+// const name = 'wes';
+// const middle = 'topher';
+// const last = 'bos';
 
-const sentence = "she's so cool";
+// const sentence = "she's so cool";
 
-const song = `Ohh
+// const song = `Ohh
 
-ya
+// ya
 
-I like
+// I like
 
-Pizza`;
+// Pizza`;
 
-const hello = "hello my name is" + name + ." Nice to meet you";
+// const hello = "hello my name is" + name + ." Nice to meet you";
 
-let hello2 = `hello my name is `;
-hello2 = hello2 + name;
-hello2 = hello2 + ` Nice to meet you`;
+// let hello2 = `hello my name is `;
+// hello2 = hello2 + name;
+// hello2 = hello2 + ` Nice to meet you`;
+
+// const person = {
+//     first: `wes`,
+//     last: `bos`,
+//     age: 100
+// };
+
+// let dog;
+// console.log(dog);
+// dog = `snickers`;
+
+// let somethingUndefined;
+// const somethingNull = null;
+
+// const cher = {
+//     first: `cher`
+// };
+
+// const teller = {
+//     first: `Teller`,
+//     last: `Teller`
+// };
+
+// teller.first = `Teller`;
+// teller.last = null;
+
+let isDrawing = false;
+const age = 18
+const ofAge = age > 19;
+console.log(ofAge);
+
+age2 = 100;
