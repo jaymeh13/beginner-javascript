108b8810d469d7108c7af835a42ff5293c2dbab0
Jayme Hoffman <jaymeh13@gmail.com>
2024-07-22 14:12:07 -0500

    Completed types exercise

---
+++ playground/types.js
@@ -0,0 +1,20 @@
+debugger;
+const name = 'wes';
+const middle = 'topher';
+const last = 'bos';
+
+const sentence = "she's so cool";
+
+const song = `Ohh
+
+ya
+
+I like
+
+Pizza`;
+
+const hello = "hello my name is" + name + ." Nice to meet you";
+
+let hello2 = `hello my name is `;
+hello2 = hello2 + name;
+hello2 = hello2 + ` Nice to meet you`;
