aa9c2e26aefbd6b86428a096aa569b5155c21dd4
Jayme Hoffman <jaymeh13@gmail.com>
2024-07-23 10:32:44 -0500

    Completed different ways to declar a function

---
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -0,0 +1,46 @@
+// function doctorize(firstName) {
+//     return `Dr. ${firstName}`;
+// }
+
+// // Anon function
+// function (firstName) {
+//     return `Dr. ${firstName}`;
+// }
+
+// console.log(doctorize2('Wes'));
+
+// // functon expression
+// const doctorize = function(firstName) {
+//     return `Dr. ${firstName}`;
+// }
+
+// // hoisting
+// function doctorize2(firstName) {
+//     return `Dr. ${firstName}`;
+// }
+
+// // Arrow functions
+// const inchToCM = (inches) => inches * 2.54;
+
+// // Skinny arrow ->
+// // Fat arrow =>
+
+// // function add(a, b = 3) {
+// //     const total = a + b;
+// //     return total;
+// // }
+
+// // const add = (a, b = 3) => a + b;
+
+// // const makeABaby = (first, last) => ({ name: `${first} ${last}`, age: 0 });
+
+// // iife
+// // immediately invoked function
+// // Why? popular before block scope bc handy
+// function() {
+//     console.log('Running the anon function');
+//     return 'you are cool';
+// }
+
+// Callback function
+button.addEventListner
